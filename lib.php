dd574578284603b2850af04ff82698a6816b73d9
Jamie Pratt <me@jamiep.org>
2012-06-19 20:34:12 +0700

    NOBUG change in file access api in 2.3

--- lib.php
+++ lib.php
@@ -31,8 +31,8 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Checks file access for short answer questions.
  */
-function qtype_varnumeric_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload) {
+function qtype_varnumeric_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options=array()) {
     global $DB, $CFG;
     require_once($CFG->libdir . '/questionlib.php');
-    question_pluginfile($course, $context, 'qtype_varnumeric', $filearea, $args, $forcedownload);
+    question_pluginfile($course, $context, 'qtype_varnumeric', $filearea, $args, $forcedownload, $options);
 }
