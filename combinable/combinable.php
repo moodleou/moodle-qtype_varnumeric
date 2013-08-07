14d11b3b84f99073e6a2e8f47a8ab7524628250b
Tim Hunt <T.J.Hunt@open.ac.uk>
2013-08-07 19:38:16 +0100

    Fix incorrect PARAM_TEXT.

--- combinable/combinable.php
+++ combinable/combinable.php
@@ -62,8 +62,8 @@ class qtype_combined_combinable_varnumeric extends qtype_combined_combinable_tex
                                                  $this->form_field_name('error[0]'),
                                                  get_string('error', 'qtype_varnumericset'),
                                                  array('size' => 16));
-        $mform->setType($this->form_field_name('answer'), PARAM_TEXT);
-        $mform->setType($this->form_field_name('error'), PARAM_TEXT);
+        $mform->setType($this->form_field_name('answer'), PARAM_RAW);
+        $mform->setType($this->form_field_name('error'), PARAM_RAW);
         $mform->addElement('group',
                            $this->form_field_name('answergroup'),
                            get_string('answer', 'question'),
